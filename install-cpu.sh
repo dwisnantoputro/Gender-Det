d6a2d98a5a744058c34b5899abf2fd60450afceb
dwisnantoputro <dwisnantoputro@gmail.com>
2022-01-22 10:46:38 +0900

    Add files via upload

---
+++ install-cpu.sh
@@ -0,0 +1,7 @@
+pip install torch==1.3.0+cpu torchvision==0.4.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+pip install opencv-python && pip install IPython && pip install matplotlib==3.1.3  && pip install torchsummary && pip install pillow==6 && pip install ptflops
+pip install keras==2.3.1
+pip install tensorflow==2.0.0
+pip install keras-layer-normalization==0.14.0
+pip install h5py==2.10.0 
+pip install imutils
